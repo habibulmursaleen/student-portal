5c45e7d99cabbdb7bfd5fda613ef6a64fd31f9b0
habibulmursaleen <habibulmursaleen97@gmail.com>
2023-08-13 00:00:10 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -1,19 +1,11 @@
-const auth = require("json-server-auth");
 const jsonServer = require("json-server");
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 9000;
-
-server.db = router.db;
+const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
-
-const rules = auth.rewriter({});
-
-server.use(rules);
-server.use(auth);
 server.use(router);
 
 server.listen(port);
