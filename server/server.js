3ea44ec0f48517b5971642df4c0c27d043a74025
habibulmursaleen <habibulmursaleen97@gmail.com>
2023-08-12 23:43:12 +0200

    server deply fix

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
+const auth = require("json-server-auth");
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // <== Will be created later
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 9000;
+
+server.db = router.db;
 
 server.use(middlewares);
+
+const rules = auth.rewriter({});
+
+server.use(rules);
+server.use(auth);
 server.use(router);
 
 server.listen(port);
